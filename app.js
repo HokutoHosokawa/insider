00cf7032c9e71d4795a1303c6d0e30623ad78992
HokutoHosokawa <h2430118@edu.cc.uec.ac.jp>
2024-08-17 16:05:10 +0900

    投票機能の追加

--- app.js
+++ app.js
@@ -117,12 +117,10 @@ app.post("/verify-password/:roomID", (req, res) => {
 
 io.on("connection", (socket) => {
     let timerInterval;
-    console.log("A user connected");
 
     socket.on("join-room", (data) => {
         const {roomID, userID, username} = data;
         const room1 = room[roomID];
-        console.log(room);
         if(room1.occupants === null || room1.occupants === undefined || room1.occupants === NaN){
             room1.occupants = 0;
         }
@@ -130,7 +128,7 @@ io.on("connection", (socket) => {
             room1.occupants += 1;
             room1.users[userID] = username;
             socket.join(roomID);
-            io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users});
+            io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users, state: "join", userName: room1.users[userID]});
         }
     });
 
@@ -153,6 +151,8 @@ io.on("connection", (socket) => {
         const csvFilePath = path.join(__dirname, "words.csv");
         try {
             const word = await getRandomWordFromCSV(csvFilePath);
+            room1.insider = insider;
+            room1.gameMaster = gameMaster;
             io.to(roomID).emit("game-started", {gamemaster: gameMaster, insider: insider, occupants: room1.users, target: word});
         } catch (error) {
             console.error(error);
@@ -174,18 +174,19 @@ io.on("connection", (socket) => {
     
     socket.on("stop-timer", (roomID) => {
         clearInterval(timerInterval);
-        io.to(roomID).emit("timer-stopped");
+        io.to(roomID).emit("timer-stopped", {gameMaster: room[roomID].gameMaster, insider: room[roomID].insider, occupants: room[roomID].users});
     });
 
     socket.on("stop-conf-timer", (roomID) => {
         clearInterval(timerInterval);
-        io.to(roomID).emit("conf-timer-stopped");
+        io.to(roomID).emit("conf-timer-stopped", {gameMaster: room[roomID].gameMaster, occupants: room[roomID].users});
     });
 
     socket.on("answer-found", (data) => {
         clearInterval(timerInterval);
         start = data.start;
         startTime = Date.now();
+        io.to(data.roomID).emit("found-answer");
         timerInterval = setInterval(() => {
             const currentTime = Date.now();
             const elapsedTime = Math.floor((currentTime - startTime)/1000);
@@ -194,20 +195,73 @@ io.on("connection", (socket) => {
         }, 333);
     });
 
+    socket.on("vote", (data) => {
+        const roomID = data.roomID;
+        const vote_user = data.vote_user;
+        const target = data.target;
+        let vote_finished = 0;
+        if(room[roomID].voteuser === undefined || room[roomID].voteuser === null || room[roomID].voteuser === NaN){
+            room[roomID].voteuser = {};
+            target.forEach((key) => {room[roomID].voteuser[key] = 0;});
+        }
+        if(room[roomID].voteuser[vote_user] === undefined || room[roomID].voteuser[vote_user] === null || room[roomID].voteuser[vote_user] === NaN){
+            room[roomID].voteuser[vote_user] = 1;
+        }else{
+            room[roomID].voteuser[vote_user] += 1;
+        }
+        for(let key in room[roomID].voteuser){
+            vote_finished += room[roomID].voteuser[key];
+        }
+        if(vote_finished === room[roomID].occupants){
+            vote_finished = 0;
+            const voteUser = room[roomID].voteuser;
+            const maxVotes = Math.max(...Object.values(voteUser));
+            const topVotedUsers = Object.keys(voteUser).filter((key) => voteUser[key] === maxVotes);
+            delete room[roomID].voteuser;
+            io.to(roomID).emit("vote-result", {gameMaster: room[roomID].gameMaster, insider: room[roomID].insider, occupants: room[roomID].users, vote_result: topVotedUsers, detail: voteUser});
+        }
+    });
+
+    socket.on("revote", (data) => {
+        const roomID = data.roomID;
+        const vote_user = data.vote_user;
+        const target = data.target;
+        let vote_finished = 0;
+        if(room[roomID].voteuser === undefined || room[roomID].voteuser === null || room[roomID].voteuser === NaN){
+            room[roomID].voteuser = {};
+            target.forEach((key) => {room[roomID].voteuser[key] = 0;});
+        }
+        if(room[roomID].voteuser[vote_user] === undefined || room[roomID].voteuser[vote_user] === null || room[roomID].voteuser[vote_user] === NaN){
+            room[roomID].voteuser[vote_user] = 1;
+        }else{
+            room[roomID].voteuser[vote_user] += 1;
+        }
+        for(let key in room[roomID].voteuser){
+            vote_finished += room[roomID].voteuser[key];
+        }
+        if(vote_finished === room[roomID].occupants){
+            vote_finished = 0;
+            const voteUser = room[roomID].voteuser;
+            const maxVotes = Math.max(...Object.values(voteUser));
+            const topVotedUsers = Object.keys(voteUser).filter((key) => voteUser[key] === maxVotes);
+            delete room[roomID].voteuser;
+            io.to(roomID).emit("revote-result", {gameMaster: room[roomID].gameMaster, insider: room[roomID].insider, occupants: room[roomID].users, vote_result: topVotedUsers, detail: voteUser});
+        }
+    });
+
     
     socket.on("leave-room", (data) => {
         const {roomID, userID} = data;
         const room1 = room[roomID];
         if(room1){
             room1.occupants -= 1;
+            const userName = room1.users[userID];
             delete room1.users[userID];
-            console.log("User left room");
             if(room1.occupants <= 0){
                 delete room[roomID];
-                console.log("Room deleted");
             }
             socket.leave(roomID);
-            io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users});
+            io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users, state: "leave", userName: userName});
         }
     });
     
@@ -219,21 +273,20 @@ io.on("connection", (socket) => {
             if(room1){
                 const userID = Object.keys(room1.users).find(key => socket.rooms[key]);
                 room1.occupants -= 1;
+                const userName = room1.users[userID];
                 if(userID){
                     delete room1.users[userID];
                 }
                 if(room1.occupants <= 0){
                     delete room[roomID];
-                    console.log("Room deleted");
                 }
-                io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users});
+                io.to(roomID).emit("occupants", {occupants: room1.occupants, users: room1.users, state: "leave", userName: userName});
             }
         });
     });
 
     socket.on("disconnecting", () => {
         console.log("A user disconnected");
-        console.log(room);
     });
 });
 
